85aeb272a5c87e2082ce3040d1109d8494579450
nikhilm2899 <nikhil.muriki@arranttech.com>
2026-01-21 17:28:04 -0600

    App File Changes

--- Clean-Room-Project/src/App.tsx
+++ Clean-Room-Project/src/App.tsx
@@ -1,8 +1,3 @@
-<<<<<<< Updated upstream
-import { useState } from "react";
-import "./App.css";
-=======
->>>>>>> Stashed changes
 
 import Navbar from "./components/navbar"
 import HeroPage from "./components/heroSection/heropage"
@@ -11,14 +6,10 @@ import HeroPage from "./components/heroSection/heropage"
 export default function App() {
   return (
     <>
-<<<<<<< Updated upstream
-      <p> ------ Clean Room Project ------</p>
-=======
       <Navbar />
       <main className="pt-[180px]">
         <HeroPage />
       </main>
->>>>>>> Stashed changes
     </>
   )
 }

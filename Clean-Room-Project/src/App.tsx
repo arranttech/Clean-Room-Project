9387eec735a40291d546d0ab3ab082760c65d7f3
sandman5445 <sandeep.mandala@arranttech.com>
2026-01-23 13:24:18 -0600

    App JS Fixed

--- Clean-Room-Project/src/App.tsx
+++ Clean-Room-Project/src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/LandingPage";
 import CustomerInfoPage from "./pages/customerInfo/CustomerInfoPage";
 import Dashboard from "./components/dashboard/dashboard";
-import Standard from "./pages/Standards/standard";
+// import Standard from "./pages/Standards/standard";
 
 function App() {
 	return (
@@ -10,8 +10,8 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/customer-info" element={<CustomerInfoPage />} />
-				<Route path="/dashboard" element={<Dashboard/>} />
-				<Route path="/standards" element={<Standard/>} />
+				<Route path="/dashboard" element={<Dashboard />} />
+				{/* <Route path="/standards" element={<Standard />} /> */}
 			</Routes>
 		</>
 	);

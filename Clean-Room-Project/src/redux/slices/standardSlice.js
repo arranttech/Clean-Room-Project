808fa13012667611c4c1b89f94067e4340155127
nikhilm2899 <nikhil.muriki@arranttech.com>
2026-02-11 11:26:08 -0600

    Standards Page Redux

---
+++ Clean-Room-Project/src/redux/slices/standardSlice.js
@@ -0,0 +1,45 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  standard: "",
+  classification: "",
+  acph: "",
+  system: "",
+  systemType: "",
+  heatingMethod: "",
+  coolingMethod: "",
+  tempUnit: "C",
+  reqInsideTempC: "",
+  reqInsideTempDisplay: "",
+  reqInsideHum: "",
+  flowVelocity: 1.5,
+  heatingFlowVelocity: 1.5,
+  coolingFlowVelocity: 1.5,
+};
+
+const standardsSlice = createSlice({
+  name: "standards",
+  initialState,
+  reducers: {
+    updateStandardsField: (state, action) => {
+      const { field, value } = action.payload;
+      state[field] = value;
+    },
+
+    updateMultipleStandardsFields: (state, action) => {
+      Object.entries(action.payload).forEach(([key, value]) => {
+        state[key] = value;
+      });
+    },
+
+    resetStandards: () => initialState,
+  },
+});
+
+export const {
+  updateStandardsField,
+  updateMultipleStandardsFields,
+  resetStandards,
+} = standardsSlice.actions;
+
+export default standardsSlice.reducer;

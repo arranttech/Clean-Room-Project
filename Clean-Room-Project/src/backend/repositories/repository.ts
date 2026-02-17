9b41bb4a7d780a406b307ada38e3e6c9398d5676
sandman5445 <sandeep.mandala@arranttech.com>
2026-02-17 16:33:04 -0600

    Adds backend and frontend support for room standards API

--- Clean-Room-Project/src/backend/repositories/repository.ts
+++ Clean-Room-Project/src/backend/repositories/repository.ts
@@ -2,10 +2,11 @@ import { database } from '../dbConnection/connections';
 
 export const ApplicationRepository = {
   createApplication: async (payload: any) => {
+    console.log('payload in repository:', payload);
     try {
       const [result] = await database.execute(
         `INSERT INTO tCustomers 
-        (customer_name, customer_phone, customer_address, customer_additional_notes, customer_email_id)
+        (customer_name, customer_phone, customer_address, customers_additional_notes, customer_email_id)
         VALUES (?, ?, ?, ?, ?)`,
         [
           payload.customerName,
@@ -16,12 +17,61 @@ export const ApplicationRepository = {
         ]
       );
 
-      // @ts-ignore
+
+      return result.insertId;
+    } catch (err) {
+      console.error('Error in createApplication:', err);
+      throw err;
+    }
+  },
+
+    roomStandards: async (payload: any) => {
+    try {
+      const [result] = await database.execute(
+        `INSERT INTO tRoomStandards 
+            (project_system,
+            project_system_type,
+            project_heating_method,
+            project_cooling_method,
+            project_standard,
+            project_classification_name,
+            project_ACPH,
+            project_temp_unit,
+            project_required_inside_temp,
+            project_required_inside_humid,
+            project_max_temp,
+            project_min_temp,
+            project_relative_min_humid,
+            project_relative_max_humid,
+            flow_velocity) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+        [
+          payload.system || null,
+          payload.systemType || null,
+          payload.heatingMethod || null,
+          payload.coolingMethod || null,
+          payload.standard || null,
+          payload.classification || null,
+          payload.acph || null,
+          payload.tempUnit || null,
+          payload.reqInsideTempC || null,
+          payload.reqInsideHum || null,
+          payload.maxTempC || null,
+          payload.minTempC || null,
+          payload.rhMin || null,
+          payload.rhMax || null,
+          payload.flowVelocity || null,
+          payload.flowMedium || null,
+          payload.heatingFlowVelocity || null,
+          payload.coolingFlowVelocity || null,
+        ]
+      );
+
       return result.insertId;
     } catch (err) {
       console.error('Error in createApplication:', err);
       throw err; // this will trigger Hapi 500
     }
   },
-  
+
 };

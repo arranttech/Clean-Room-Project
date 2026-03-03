f3cd653bdd168f05ba5f6c9c28670d3dd7b1ead4
nikhilm2899 <nikhil.muriki@arranttech.com>
2026-03-03 14:38:24 -0600

    CRUD OPERATIONS IN ADMIN CUSTOMERS

--- Clean-Room-Project/src/backend/controller/customerController.ts
+++ Clean-Room-Project/src/backend/controller/customerController.ts
@@ -5,10 +5,16 @@ export const customerInfo = (payload: object) =>
 	request("/v1/customerinfo", "POST", payload);
 
 export const getCustomerById = (customerId: number) =>
-	request(`/v1/customerinfo?customer_id=${customerId}`);
+	request(`/v1/customers/${customerId}`);
 
 export const customerDetails = (adminId?: number) =>
 	request(`/v1/customers${adminId ? `?admin_id=${adminId}` : ""}`);
 
 export const getCustomerInfo = (userLoginId: number) =>
 	request(`/v1/customers/user/${userLoginId}`);
+
+export const updateCustomer = (customerId: number, payload: object) =>
+	request(`/v1/customers/${customerId}`, "PUT", payload);
+
+export const deleteCustomer = (customerId: number) =>
+	request(`/v1/customers/${customerId}`, "DELETE");
